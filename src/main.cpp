000103a6b1d757a6156f2608f66c26a5ea758842
dami3n1 <221747455+dami3n1@users.noreply.github.com>
2026-05-11 19:42:43 -0500

    Refactor cube rotation logic for improved animation timing

--- src/main.cpp
+++ src/main.cpp
@@ -356,16 +356,13 @@ int main()
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
+
+            if (i % 3 == 0)
+                angle = glfwGetTime() * 50.0f;
+
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             int modelLoc = glGetUniformLocation(ourShader.ID, "model");
             glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-            if (i % 3 == 0)
-            {
-                model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-                glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-                glDrawArrays(GL_TRIANGLES, 0, 36);
-            }
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
